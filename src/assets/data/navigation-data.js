9eb56cb3acfbb39625123423e02ded6a73a40471
Luc Longin <luc.longin@gmail.com>
2023-07-15 01:34:37 +0700

    created nav component

---
+++ src/assets/data/navigation-data.js
@@ -0,0 +1,13 @@
+export const navLinks = [
+    {title: 'Home', url: '/'},
+    {title: 'Menu', url: '/menu'},
+    {title: 'About Us', url: '/about-us'},
+    {title: 'Contact', url: '/contact'},
+    {title: 'Drop Me', 
+        dropdown: [
+            {title: 'Drop1',url: '/drop1'},
+            {title: 'Drop2',url: '/drop2'},
+            {title: 'Drop3',url: '/drop3'}
+        ]
+    }
+];
